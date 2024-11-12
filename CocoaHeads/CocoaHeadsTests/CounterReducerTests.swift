e9a607e8a93d0ccc6e19d3174353b13074d2f7d4
Ben Sarrazin <b@srz.io>
2024-11-12 11:51:06 -0500

    Base project with placeholders

---
+++ CocoaHeads/CocoaHeadsTests/CounterReducerTests.swift
@@ -0,0 +1,8 @@
+@testable import CocoaHeads
+import Testing
+
+struct CounterReducerTests {
+
+    @Test
+    func test0() async throws { }
+}
---
+++ CocoaHeads/CocoaHeadsTests/CounterViewModelTests.swift
@@ -0,0 +1,8 @@
+@testable import CocoaHeads
+import Testing
+
+struct CounterViewModelTests {
+
+    @Test
+    func test0() async throws { }
+}
