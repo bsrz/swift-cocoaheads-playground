85b90686b915d28b48b2241e9c5ceab2b3574e16
Ben Sarrazin <b@srz.io>
2024-11-12 12:15:24 -0500

    Adding simple counter view model

--- CocoaHeads/CocoaHeads/Source/MVVM/CounterViewModel.swift
+++ CocoaHeads/CocoaHeads/Source/MVVM/CounterViewModel.swift
@@ -1,1 +1,47 @@
+import Observation
+import SwiftUI
+
+@Observable
+class CounterViewModel {
+
+    struct State: Equatable {
+        var count: Int = 0
+    }
+
+    private(set) var state = State()
+
+    enum Action {
+        case decrement
+        case increment
+    }
+
+    func send(_ action: Action) {
+        switch action {
+        case .decrement:
+            state.count -= 1
+
+        case .increment:
+            state.count += 1
+        }
+    }
+}
+
+struct CounterViewModelView: View {
+
+    @State var viewModel = CounterViewModel()
+
+    var body: some View {
+        VStack {
+            HStack {
+                Button("-") { viewModel.send(.decrement) }
+                Text("\(viewModel.state.count)")
+                Button("+") { viewModel.send(.increment) }
+            }
+        }
+    }
+}
+
+#Preview {
+    CounterViewModelView()
+}
 
