9e08e69c2e237b15d72c31fb734b6835b470aea5
Ben Sarrazin <b@srz.io>
2024-11-12 12:36:36 -0500

    Adding tests to CounterViewModel

--- CocoaHeads/CocoaHeadsTests/CounterViewModelTests.swift
+++ CocoaHeads/CocoaHeadsTests/CounterViewModelTests.swift
@@ -4,5 +4,38 @@ import Testing
 struct CounterViewModelTests {
 
     @Test
-    func test0() async throws { }
+    func testGivenInitialState_whenNoActions_thenCountIsZero() {
+        let sut = CounterViewModel()
+        #expect(sut.state.count == 0)
+    }
+
+    @Test
+    func testGivenInitialState_whenDecrementAction_thenCountIsMinusOne() {
+        let sut = CounterViewModel()
+        sut.send(.decrement)
+        #expect(sut.state.count == -1)
+    }
+
+    @Test
+    func testGivenInitialState_whenIncrementAction_thenCountIsOne() {
+        let sut = CounterViewModel()
+        sut.send(.increment)
+        #expect(sut.state.count == 1)
+    }
+
+    @Test
+    func testGivenInitialState_whenRandomActions_thenCountIsExpected() {
+        let decrements = Int.random(in: 0..<100)
+        let increments = Int.random(in: 0..<100)
+        let sut = CounterViewModel()
+
+        for _ in 0...decrements {
+            sut.send(.decrement)
+        }
+        for _ in 0...increments {
+            sut.send(.increment)
+        }
+
+        #expect(sut.state.count == increments - decrements)
+    }
 }
